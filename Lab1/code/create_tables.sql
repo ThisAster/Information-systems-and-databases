86ad5d78376b14ccc4333259abcd3946223537ff
ThisAster <freizen034@gmail.com>
2023-09-23 05:57:11 +0300

    first commit

---
+++ Lab1/code/create_tables.sql
@@ -0,0 +1,56 @@
+CREATE TABLE IF NOT EXISTS Flock_Characteristic (
+    f_characteristic_id SERIAL PRIMARY KEY NOT NULL,
+    flock_characteristic_name VARCHAR(255) NOT NULL check ( length(flock_characteristic_name) > 0 )
+);
+
+CREATE TABLE IF NOT EXISTS Movement_Flock (
+    movement_flock_id SERIAL PRIMARY KEY NOT NULL,
+    from_place_name VARCHAR(100) NOT NULL check ( length(from_place_name) > 0 ), 
+    to_place_name VARCHAR(100) NOT NULL check ( length(to_place_name) > 0 )
+);
+
+CREATE TABLE IF NOT EXISTS Flock_Action (
+    flock_action_id SERIAL PRIMARY KEY NOT NULL,
+    flock_action_name VARCHAR(255) NOT NULL check ( length(flock_action_name) > 0 )
+);
+
+CREATE TABLE IF NOT EXISTS Weapon (
+    weapon_id SERIAL PRIMARY KEY NOT NULL,
+    weapon_name VARCHAR(100) NOT NULL 
+);
+
+CREATE TABLE IF NOT EXISTS Leader_Action (
+    leader_action_id SERIAL PRIMARY KEY NOT NULL,
+    leader_action_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Leader (
+    leader_id SERIAL PRIMARY KEY NOT NULL,
+    leader_name VARCHAR(100) NOT NULL check ( length(leader_name) > 0 ),
+    leader_action_id INT REFERENCES leader_action (leader_action_id),
+    weapon_id INT REFERENCES weapon (weapon_id)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS Tribesman (
+    tribesman_id SERIAL PRIMARY KEY,
+    tribesman_name VARCHAR(100),
+    weapon_id INT REFERENCES weapon (weapon_id),
+    leader_id INT REFERENCES leader (leader_id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Flock (
+    flock_id SERIAL PRIMARY KEY NOT NULL,
+    flock_name VARCHAR(100) NOT NULL check ( length(flock_name) > 0 ),
+    leader_id INT REFERENCES leader (leader_id) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Flock_to_Characteristic (
+    flock_id INT REFERENCES flock(flock_id) NOT NULL,
+    f_characteristic_id INT REFERENCES flock_characteristic (f_characteristic_id) NOT NULL
+);
+
+
+
+
---
+++ Lab1/code/insertion.sql
@@ -0,0 +1,124 @@
+INSERT INTO Flock_Characteristic(flock_characteristic_name)
+VALUES ('голодная');
+INSERT INTO Flock_Characteristic(flock_characteristic_name)
+VALUES ('удивленная');
+INSERT INTO Flock_Characteristic(flock_characteristic_name)
+VALUES ('агрессивная');
+INSERT INTO Flock_Characteristic(flock_characteristic_name)
+VALUES ('усталая');
+INSERT INTO Flock_Characteristic(flock_characteristic_name)
+VALUES ('радостная');
+
+
+INSERT INTO Movement_Flock(from_place_name, to_place_name)
+VALUES ('озеро', 'тропинка');
+INSERT INTO Movement_Flock(from_place_name, to_place_name)
+VALUES ('тропинка', 'поле');
+INSERT INTO Movement_Flock(from_place_name, to_place_name)
+VALUES ('поле', 'вход в пещеру');
+INSERT INTO Movement_Flock(from_place_name, to_place_name)
+VALUES ('вход в пещеру', 'выход в пещеру');
+INSERT INTO Movement_Flock(from_place_name, to_place_name)
+VALUES ('выход в пещеру', 'лес');
+
+
+INSERT INTO Flock_Action(flock_action_name)
+VALUES ('удивляется');
+INSERT INTO Flock_Action(flock_action_name)
+VALUES ('поедает');
+INSERT INTO Flock_Action(flock_action_name)
+VALUES ('радуется');
+INSERT INTO Flock_Action(flock_action_name)
+VALUES ('спит');
+INSERT INTO Flock_Action(flock_action_name)
+VALUES ('атакует');
+
+
+INSERT INTO Weapon(weapon_name)
+VALUES ('палка');
+INSERT INTO Weapon(weapon_name)
+VALUES ('камень');
+INSERT INTO Weapon(weapon_name)
+VALUES ('копье');
+INSERT INTO Weapon(weapon_name)
+VALUES ('факел');
+
+
+INSERT INTO Leader_Action(leader_action_name)
+VALUES ('направляет стаю');
+INSERT INTO Leader_Action(leader_action_name)
+VALUES ('атакует кабана');
+INSERT INTO Leader_Action(leader_action_name)
+VALUES ('следит за безопасностью стаи');
+
+
+INSERT INTO Leader(leader_name, leader_action_id, weapon_id)
+VALUES ('Лопоухий', 1, NULL);
+INSERT INTO Leader(leader_name, leader_action_id, weapon_id)
+VALUES ('Длинноухий', 2, 2);
+INSERT INTO Leader(leader_name, leader_action_id, weapon_id)
+VALUES ('Наблюдающий', 3, NULL);
+INSERT INTO Leader(leader_name, leader_action_id, weapon_id)
+VALUES ('Стражник', 3, 4);
+
+
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES ('первый сородич', 3, 2);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES ('второй сородич', 1, 1);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES ('третий сородич', 3, 2);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 2);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 3);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 1);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 2);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 2);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 4);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 4);
+INSERT INTO Tribesman(tribesman_name, weapon_id, leader_id)
+VALUES (NULL, NULL, 4);
+
+
+INSERT INTO Flock(flock_name, leader_id)
+VALUES('первая стая', 2);
+INSERT INTO Flock(flock_name, leader_id)
+VALUES('вторая стая', 3);
+INSERT INTO Flock(flock_name, leader_id)
+VALUES('третья стая', 4);
+INSERT INTO Flock(flock_name, leader_id)
+VALUES('четвертая стая', 1);
+
+
+INSERT INTO Flock_to_Characteristic
+SELECT 1, 3
+WHERE
+    NOT EXISTS (
+        SELECT flock_id, f_characteristic_id FROM Flock_to_Characteristic WHERE flock_id = 1 and f_characteristic_id = 3
+    );
+INSERT INTO Flock_to_Characteristic
+SELECT 2, 4
+WHERE
+    NOT EXISTS (
+        SELECT flock_id, f_characteristic_id FROM Flock_to_Characteristic WHERE flock_id = 2 and f_characteristic_id = 4
+    );
+INSERT INTO Flock_to_Characteristic
+SELECT 3, 1
+WHERE
+    NOT EXISTS (
+        SELECT flock_id, f_characteristic_id FROM Flock_to_Characteristic WHERE flock_id = 3 and f_characteristic_id = 1
+    );
+INSERT INTO Flock_to_Characteristic
+SELECT 4, 5
+WHERE
+    NOT EXISTS (
+        SELECT flock_id, f_characteristic_id FROM Flock_to_Characteristic WHERE flock_id = 4 and f_characteristic_id = 5
+    );
+
+-- SELECT * FROM Flock INNER JOIN Leader ON flock.flock_id = leader.leader_id INNER join Tribesman ON leader.leader_id = tribesman.tribesman_id WHERE tribesman.weapon_id = 4;
